11de409e435b6eb53d8c1ddfc3e6904f9179c92f
vimalAdithan <vimaladithanrayz1213s.v.t@gmail.com>
2023-03-21 19:25:35 +0530

    connection complete

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv' 
 dotenv.config()
 import express from "express";
-import { MongoClient } from "mongodb";
 import uniqid from 'uniqid';
 const app = express();
-
-const MONGO_URL = "mongodb://127.0.0.1";
 const PORT = process.env.PORT;
-const client = new MongoClient(MONGO_URL); // dial
-// Top level await
-await client.connect(); // call
-console.log("Mongo is connected !!!  ");
+
 const booking = [
   {
     customer_name: "bharath",
@@ -72,12 +66,12 @@ const booking = [
   },
 ];
 
-app.get("/", async function (request, response) {
+app.get("/",function (request, response) {
   response.send(booking);
 });
 
 // list all rooms with booked data
-app.get("/booked", async function (request, response) {
+app.get("/booked",function (request, response) {
   const result = booking.map((e) => ({
     room_name: `${e.room_name}`,
     booking_status: `${e.booking_status}`,
@@ -90,7 +84,7 @@ app.get("/booked", async function (request, response) {
 });
 
 // list all customers with booked data
-app.get("/customers", async function (request, response) {
+app.get("/customers",function (request, response) {
   const result = booking.map((e) => ({
     room_name: `${e.room_name}`,
     customer_name: `${e.customer_name}`,
@@ -102,7 +96,7 @@ app.get("/customers", async function (request, response) {
 });
 
 // list a customer with booked data
-app.get("/:id", async function (request, response) {
+app.get("/:id",function (request, response) {
   const { id } = request.params;
   let result = booking
     .filter((e) => e.id == id)
@@ -120,7 +114,7 @@ app.get("/:id", async function (request, response) {
 });
 
 // book a room
-app.post("/add", async function (request, response) {
+app.post("/add",function (request, response) {
   // const {id}=request.params;
   const add={
     customer_name: "bharath",
